5eeb6c036915e6dc3fdb94111dc0d7dc003695b6
avni chauhan <avnichauhan9182@gmail.com>
2023-02-19 00:02:55 +0530

    first commit

---
+++ src/main/java/devops/b2/lab4/jenkin/Myclass.java
@@ -0,0 +1,13 @@
+package devops.b2.lab4.jenkin;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=9;
+		
+		System.out.println(a+b);
+
+	}
+
+}
